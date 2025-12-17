c16d1b4059d91bad43cc9a052c835ff086bb1f90
faustinopsy <rodrigohipnose@gmail.com>
2025-12-17 18:17:32 -0300

    nano RAG  iniciando um teste

---
+++ src/VectorDb/Check.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NanoRag\VectorDb;
+
+class Check
+{
+    public function sayHello(): string
+    {
+        return "Ambiente Configurado: Namespace NanoRag\\VectorDb carregado com sucesso.";
+    }
+}
